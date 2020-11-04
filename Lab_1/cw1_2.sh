2961582dd7b8f8c5c0ff3aee88c6c4b594b3aa4e
Matthias Marczyszyn <m.marczyszyn@gmail.com>
2020-11-04 18:19:26 +0100

    remove form Lab2 Lab1

--- Lab_1/cw1_2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-filename='adresy.txt'
-while read email; do
-mutt -s "PCV" $email < tekst.txt -a Spec*.pdf
-done < $filename
--- Lab_1/cw1_3.sh
+++
@@ -1,3 +0,0 @@
- #!/bin/bash
- find ./ -iname "*$1" -atime -$2 -exec tar -rvf $3.tar {} \;
-
